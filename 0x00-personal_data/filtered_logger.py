0479b54ce4c2ea13ed4dda6f46897a1ec36008ca
ShogoMark <trigmark87@gmail.com>
2023-08-03 15:23:26 +0100

    task1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,3 +10,20 @@ def filter_datum(fields: str, redaction: str,
                                                          re.escape(separator))
                              for field in fields)
     return re.sub(regex_pattern, redaction, message)
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        for field in self.fields:
+            record.msg = filter_datum([field], self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
