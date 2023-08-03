37d366e951b446b5495c2057153ab2b06bce10d2
ShogoMark <trigmark87@gmail.com>
2023-08-03 14:27:36 +0100

    task0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import re
 
-def filter_datum(fields: str, redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: str, redaction: str,
+                 message: str, separator: str) -> str:
     """obfuscate specified field with redaction string as separator"""
     regex_pattern = '|'.join(r'(?<=\b{}\=)[^{}]+'.format(re.escape(field),
                                                          re.escape(separator))
-                                                         for field in fields)
+                             for field in fields)
     return re.sub(regex_pattern, redaction, message)
