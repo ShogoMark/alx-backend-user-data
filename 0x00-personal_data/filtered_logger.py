cfba171770b88bc8b48a414b314c22faf910351f
ShogoMark <trigmark87@gmail.com>
2023-08-04 14:36:16 +0100

    taskedd

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import re
 import logging
+from typing import List
 
 
-def filter_datum(fields: str, redaction: str,
+def filter_datum(fields: typing.List[str], redaction: str,
                  message: str, separator: str) -> str:
     """obfuscate specified field with redaction string as separator"""
     regex_pattern = '|'.join(r'(?<=\b{}\=)[^{}]+'.format(re.escape(field),
@@ -31,3 +32,21 @@ class RedactingFormatter(logging.Formatter):
             record.msg = filter_datum([field], self.REDACTION,
                                       record.msg, self.SEPARATOR)
         return super().format(record)
+
+def get_logger():
+    """Returns a logging.Logger object with desired config."""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+
+    PII_FIELDS = ('name', 'email', 'phone', 'address', 'credit_card')
+
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    logger.propagate = False
+
+    return logger
