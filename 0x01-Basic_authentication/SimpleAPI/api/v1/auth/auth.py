4487cdb1fdbd2b02faa1def11500de498849c57d
ShogoMark <trigmark87@gmail.com>
2023-08-07 20:28:05 +0100

    task4

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -3,16 +3,35 @@
 from flask import request
 from typing import List, TypeVar
 
-class Auth:
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """check if authentication is required for given path"""
-        return False
 
+class Auth:
+    def require_auth(self, path: str,
+                     excluded_paths: List[str],
+                     strict_slashes=False) -> bool:
+        """check if authentication is required for given path"""
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path.endswith("/") and excluded_path.endswith("/"):
+                if path == excluded_path:
+                    return False
+            elif path.endswith("/"):
+                if path[:-1] == excluded_path:
+                    return False
+            elif excluded_path.endswith("/"):
+                if path == excluded_path[:-1]:
+                    return False
+            elif path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
-        "method that returns None"
+        """method that returns None"""
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """method returns None"""
