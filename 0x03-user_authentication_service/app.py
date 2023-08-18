9052fd8b1ac81d7d28a55816450bcb9ff6c731e4
ShogoMark <trigmark87@gmail.com>
2023-08-18 21:04:06 +0100

    tskk

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -25,7 +25,7 @@ def users():
             return jsonify({"message": "Missing email or password"}), 400
 
         AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"}), 201
+        return jsonify({"email": email, "message": "user created"}), 200
     except ValueError as e:
         return jsonify({"message": "email already registered"}), 400
 
