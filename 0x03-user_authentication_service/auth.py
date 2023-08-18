609125d3426c1a0a72f2d0e481b1f705daad28f1
ShogoMark <trigmark87@gmail.com>
2023-08-18 11:05:56 +0100

    task5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,12 +2,36 @@
 """hashed passwords"""
 
 import bcrypt
+from db import DB
+from user import User
 
 
-def _hash_password(password: str) -> str:
-    """function returns salted hash of input password"""
-    salt = bcrypt.gensalt()
+class Auth:
+    """Auth class to interact with the authentication database"""
 
-    salted_hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    def __init__(self):
+        self._db = DB()
 
-    return salted_hashed_password
+    def register_user(self, email: str, password: str) -> User:
+        """registers a new user with email & password"""
+        user_exist = self._db.find_user_by(email=email)
+        if user_exist:
+            raise ValueError(f"User {email} already exists")
+
+        # Hash the password
+        hashed_password = self._hash_password(password)
+
+        # create and add the new user to the db
+        new_user = self._db.add_user(email=email,
+                                     hashed_password=hashed_password)
+        return new_user
+
+
+
+    def _hash_password(self, password: str) -> str:
+        """function returns salted hash of input password"""
+        salt = bcrypt.gensalt()
+
+        salted_hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+        return salted_hashed_password
