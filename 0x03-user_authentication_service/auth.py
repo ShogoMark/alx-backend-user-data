a4c0c11afe49504ed0e180312aa5b0eaa0b5268e
ShogoMark <trigmark87@gmail.com>
2023-08-18 10:21:38 +0100

    task4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""hashed passwords"""
+
+import bcrypt
+
+def _hash_password(password: str) -> str:
+	"""function returns salted hash of input password"""
+	salt = bcrypt.gensalt()
+
+	salted_hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+	return salted_hashed_password
