86c96e55756343a74f581ed309f91c94043f81b4
ShogoMark <trigmark87@gmail.com>
2023-08-21 14:07:04 +0100

    task3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,35 +6,30 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """manages authentication"""
+
     def require_auth(self, path: str,
                      excluded_paths: List[str],
                      strict_slashes=False) -> bool:
         """check if authentication is required for given path"""
-        if path is None:
+        if not path:
             return True
-
-        if excluded_paths is None or len(excluded_paths) == 0:
+        if not excluded_paths or excluded_paths == []:
             return True
-
-        for excluded_path in excluded_paths:
-            if path.endswith("/") and excluded_path.endswith("/"):
-                if path == excluded_path:
-                    return False
-            elif path.endswith("/"):
-                if path[:-1] == excluded_path:
-                    return False
-            elif excluded_path.endswith("/"):
-                if path == excluded_path[:-1]:
-                    return False
-            elif path == excluded_path:
+        if not path.endswith("/"):
+            path += "/"
+        if path in excluded_paths:
+            return False
+        for paths in excluded_paths:
+            paths = paths.rstrip("*")
+            if path.find(paths) != -1:
                 return False
         return True
 
     def authorization_header(self, request=None) -> str:
         """method that returns None"""
-        if 'Authorization' in request.headers:
-            auth_value = request.headers.get('Authorization')
-            return f'{auth_value}', 200
+        if request:
+            return request.headers.get("Authorization", None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
